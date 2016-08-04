6043655dba752fdda54a0eedcefd1bd6410985fa
HD <hrishikesh.date@gmail.com>
2016-08-04 22:01:37 +0530

    Read message from Serial input

--- sketch_blink_morse/sketch_blink_morse.ino
+++ sketch_blink_morse/sketch_blink_morse.ino
@@ -1,12 +1,14 @@
 /* Blink without Delay
 
   Light up the LED to send a Morse code for a given string
+  To get the morse code just open the Serial Terminal and type in a string and hit ENTER.
+  You should see the LED blinking with the Morse code for the String
 
   The circuit:
    LED attached from pin 13 to ground.
    Note: on most Arduinos, there is already an LED on the board
   that's attached to pin 13, so no hardware is needed for this example.
-
+  
   created 2016
   by Hrishikesh Date
 
@@ -28,10 +30,11 @@ const long dot = interval;
 const long dash = interval * 3;
 const long gapinwords = interval * 7;
 
-char alpha[CHARSUPPORTED] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-                                'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                                'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8',
-                                '9', '0', ' '};
+char alpha[CHARSUPPORTED] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                              'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                              'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8',
+                              '9', '0', ' '
+                            };
 
 String dottie [CHARSUPPORTED] = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.",
                                   "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
@@ -39,6 +42,8 @@ String dottie [CHARSUPPORTED] = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.
                                   "-.--", "--..", ".----", "..---", "...--", "....-", ".....",
                                   "-....", "--...", "---..", "----.", "-----", "|"
                                 };
+bool stringComplete = false;
+String inputString = "";
 
 void setup() {
   // set the digital pin as output:
@@ -49,15 +54,17 @@ void setup() {
 void loop() {
   // here is where you'd put code that needs to be running all the time.
 
-  //for not just use small case
-  String message = "hello world";
-  String morsedmessage = toMorse(message);
-  
-  //Signal the message
-  toSignal(morsedmessage);
-  
-  delay(10000);
+  if (stringComplete) {
+    String morsedmessage = toMorse(inputString);
+
+    inputString = "";
+    stringComplete = false;
+
+    //Signal the message
+    toSignal(morsedmessage);
+  }
 }
+
 int getindexforelement (char a)
 {
   for (int j = 0; j < CHARSUPPORTED; j++)
@@ -69,8 +76,10 @@ int getindexforelement (char a)
   }
   return -1;
 }
-String toMorse(String message)
+String toMorse(String msg)
 {
+  String message = msg;
+  message.toUpperCase();
   String morse = "";
   for (int j = 0; j < message.length(); j++)
   {
@@ -96,7 +105,7 @@ void signalandwait(int d)
   digitalWrite(ledPin, LOW);
   delay(interval);
 }
-void toSignal(String message){
+void toSignal(String message) {
   for (int j = 0; j < message.length(); j++)
   {
     if (message[j] == '.')
@@ -105,14 +114,31 @@ void toSignal(String message){
     }
     else if (message[j] == '-')
     {
-        signalandwait(dash);
+      signalandwait(dash);
     }
-    else if (message[j] == '|'){
-        signalandwait(gapinwords);
-      }
-    else if (message[j] == ' '){
+    else if (message[j] == '|') {
+      signalandwait(gapinwords);
+    }
+    else if (message[j] == ' ') {
       //Don't do any signal just wait
-      delay(interval);  
+      delay(interval);
     }
-  } 
+  }
 }
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    char inChar = (char)Serial.read();
+    // if the incoming character is a newline, set a flag
+    // so the main loop can do something about it:
+    if (inChar == '\n' || inChar == '\r' ) {
+      stringComplete = true;
+    }
+    else {
+      // add it to the inputString:
+      inputString += inChar;
+    }
+  }
+}
+
